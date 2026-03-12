813c3d7478c184c21d8e59bf0d9cab50208b2b89
Mattox0 <matbegaaar@gmail.com>
2026-03-12 23:39:22 +0100

    KYX: fix migration

--- migrations/1773353959256-migration.ts
+++ migrations/1773353959256-migration.ts
@@ -5,8 +5,7 @@ export class Migration1773353959256 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "prefer" RENAME COLUMN "mentionedUserOneGender" TO "mentionedUserGender"`);
-        await queryRunner.query(`ALTER TABLE "prefer" ALTER COLUMN "mentionedUserGender" TYPE "public"."prefer_mentionedusergender_enum" USING "mentionedUserGender"::text::"public"."prefer_mentionedusergender_enum"`);
-        await queryRunner.query(`DROP TYPE "public"."prefer_mentioneduseronegender_enum"`);
+        await queryRunner.query(`ALTER TYPE "public"."prefer_mentioneduseronegender_enum" RENAME TO "prefer_mentionedusergender_enum"`);
         await queryRunner.query(`ALTER TABLE "prefer" DROP COLUMN "mentionedUserTwoGender"`);
         await queryRunner.query(`DROP TYPE "public"."prefer_mentionedusertwogender_enum"`);
     }
@@ -14,8 +13,7 @@ export class Migration1773353959256 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TYPE "public"."prefer_mentionedusertwogender_enum" AS ENUM('MAN', 'FEMALE', 'ALL')`);
         await queryRunner.query(`ALTER TABLE "prefer" ADD "mentionedUserTwoGender" "public"."prefer_mentionedusertwogender_enum"`);
-        await queryRunner.query(`CREATE TYPE "public"."prefer_mentioneduseronegender_enum" AS ENUM('MAN', 'FEMALE', 'ALL')`);
-        await queryRunner.query(`ALTER TABLE "prefer" ALTER COLUMN "mentionedUserGender" TYPE "public"."prefer_mentioneduseronegender_enum" USING "mentionedUserGender"::text::"public"."prefer_mentioneduseronegender_enum"`);
+        await queryRunner.query(`ALTER TYPE "public"."prefer_mentionedusergender_enum" RENAME TO "prefer_mentioneduseronegender_enum"`);
         await queryRunner.query(`ALTER TABLE "prefer" RENAME COLUMN "mentionedUserGender" TO "mentionedUserOneGender"`);
     }
 
